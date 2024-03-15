0a1e3b263ec2047341e985f2c0221195737210dd
Lawrence Ezealor <ezealor20@gmail.com>
2024-03-15 02:11:53 +0100

    new commit

--- src/App.jsx
+++ src/App.jsx
@@ -9,73 +9,69 @@ import useMyNfts from "./hooks/useMyNfts";
 configureWeb3Modal();
 
 function App() {
-    const tokensData = useCollections();
-    const myTokenIds = useMyNfts();
+  const tokensData = useCollections();
+  const myTokenIds = useMyNfts();
 
-    const myTokensData = tokensData.filter((x, index) =>
-        myTokenIds.includes(index)
-    );
-    return (
-        <Container>
-            <Header />
-            <main className="mt-6">
-                <AppTabs
-                    MyNfts={
-                        <Flex align="center" gap="8" wrap={"wrap"}>
-                            {myTokensData.length === 0 ? (
-                                <Text>No NFT owned yet</Text>
-                            ) : (
-                                myTokensData.map((x) => (
-                                    <Box key={x.dna} className="w-[20rem]">
-                                        <img
-                                            src={x.image}
-                                            className="w-full object-contain"
-                                            alt={x.name}
-                                        />
-                                        <Text className="block text-2xl">
-                                            Name: {x.name}
-                                        </Text>
-                                        <Text className="block">
-                                            Description: {x.description}
-                                        </Text>
-                                        <Button className="px-8 py-2 text-xl mt-2">
-                                            Mint
-                                        </Button>
-                                    </Box>
-                                ))
-                            )}
-                        </Flex>
-                    }
-                    AllCollections={
-                        <Flex align="center" gap="8" wrap={"wrap"}>
-                            {tokensData.length === 0 ? (
-                                <Text>Loading...</Text>
-                            ) : (
-                                tokensData.map((x) => (
-                                    <Box key={x.dna} className="w-[20rem]">
-                                        <img
-                                            src={x.image}
-                                            className="w-full object-contain"
-                                            alt={x.name}
-                                        />
-                                        <Text className="block text-2xl">
-                                            Name: {x.name}
-                                        </Text>
-                                        <Text className="block">
-                                            Description: {x.description}
-                                        </Text>
-                                        <Button className="px-8 py-2 text-xl mt-2">
-                                            Mint
-                                        </Button>
-                                    </Box>
-                                ))
-                            )}
-                        </Flex>
-                    }
-                />
-            </main>
-        </Container>
-    );
+  const myTokensData = tokensData.filter((x, index) =>
+    myTokenIds.includes(index)
+  );
+  return (
+    <Container>
+      <Header />
+      <main className="mt-6">
+        <AppTabs
+          MyNfts={
+            <Flex align="center" gap="8" wrap={"wrap"}>
+              {myTokensData.length === 0 ? (
+                <Text>No NFT owned yet</Text>
+              ) : (
+                myTokensData.map((x) => (
+                  <Box key={x.dna} className="w-[20rem]">
+                    <img
+                      src={x.image}
+                      className="w-full object-contain"
+                      alt={x.name}
+                    />
+                    <Text className="block text-2xl">Name: {x.name}</Text>
+                    <Text className="block">Description: {x.description}</Text>
+
+                    <Button className="px-8 py-2 text-xl mt-2">
+                      <a
+                        target="_blank"
+                        href={`${"https://testnets.opensea.io/assets/mumbai/0xfbfa75a0acc293aaa7914e24637ca4835752307d/2"}`}
+                      >
+                        Link to Opensea
+                      </a>
+                    </Button>
+                  </Box>
+                ))
+              )}
+            </Flex>
+          }
+          AllCollections={
+            <Flex align="center" gap="8" wrap={"wrap"}>
+              {tokensData.length === 0 ? (
+                <Text>Loading...</Text>
+              ) : (
+                tokensData.map((x) => (
+                  <Box key={x.dna} className="w-[20rem]">
+                    <img
+                      src={x.image}
+                      className="w-full object-contain"
+                      alt={x.name}
+                    />
+                    <Text className="block text-2xl">Name: {x.name}</Text>
+                    <Text className="block">Description: {x.description}</Text>
+                    <Button className="px-8 py-2 text-xl mt-2">Mint</Button>
+                  </Box>
+                ))
+              )}
+            </Flex>
+          }
+        />
+      </main>
+    </Container>
+  );
 }
 
 export default App;
